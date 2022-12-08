fc8c15b30eb369a80a8ebf935b45e6af46921e27
matiasboni <matiasboni01@gmail.com>
2022-12-08 16:39:13 -0300

    Muestra de roles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+
+    def convert_role_to_s(role)
+        roles={"admin" => "Administrador","staff"=> "Personal Bancario","client" => "Cliente"}
+        roles[role]
+    end
 end
